8a51541b44473e302a6f9d05ffc6b73b671de669
Brayan <brarigoch@gmail.com>
2016-04-16 09:00:26 -0600

    modificando vista nuevo tema

--- views/javascript/new-topic.js
+++ views/javascript/new-topic.js
@@ -1,39 +1,28 @@
 $(document).ready(function() {
     $("#add").click(function() {
-        var intId = $("#buildyourform div").length + 1;
+        var intId = $("#question div").length + 1;
         var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
-        var fName = $("<input type=\"text\" class=\"fieldname\" />");
-        var fType = $("<select class=\"fieldtype\"><option value=\"checkbox\">Checked</option><option value=\"textbox\">Text</option><option value=\"textarea\">Paragraph</option></select>");
-        var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
+        var fName = $("<input  id =\"input-options\" type=\"text\" class=\"fieldname form-control\" />");
+        var removeButton = $("<button type=\"button\"  class=\"remove btn btn-remove btn-danger \" id=\"remove\"><span class=\"glyphicon glyphicon-minus\"></span></button>");
         removeButton.click(function() {
             $(this).parent().remove();
         });
         fieldWrapper.append(fName);
-        fieldWrapper.append(fType);
         fieldWrapper.append(removeButton);
-        $("#buildyourform").append(fieldWrapper);
-    });
-    $("#preview").click(function() {
-        $("#yourform").remove();
-        var fieldSet = $("<fieldset id=\"yourform\"><legend>Your Form</legend></fieldset>");
-        $("#buildyourform div").each(function() {
-            var id = "input" + $(this).attr("id").replace("field","");
-            var label = $("<label for=\"" + id + "\">" + $(this).find("input.fieldname").first().val() + "</label>");
-            var input;
-            switch ($(this).find("select.fieldtype").first().val()) {
-                case "checkbox":
-                    input = $("<input type=\"checkbox\" id=\"" + id + "\" name=\"" + id + "\" />");
-                    break;
-                case "textbox":
-                    input = $("<input type=\"text\" id=\"" + id + "\" name=\"" + id + "\" />");
-                    break;
-                case "textarea":
-                    input = $("<textarea id=\"" + id + "\" name=\"" + id + "\" ></textarea>");
-                    break;    
-            }
-            fieldSet.append(label);
-            fieldSet.append(input);
-        });
-        $("body").append(fieldSet);
+        $("#question").append(fieldWrapper);
     });
 });
+/*
+
+<div class="fieldwrapper" id="field1">
+    <input id ="input-options"type="text" class="fieldname form-control">
+    <button type="button" class="remove btn btn-remove btn-danger" id="remove">
+            <span class="glyphicon glyphicon-minus"></span>
+
+    </button>
+
+    <button type="button"  class="add btn btn-success btn-add " id="add">
+        <span class="glyphicon glyphicon-plus"></span>
+    </button>   
+</div>
+*/
