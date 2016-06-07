d0a5e6111445e44d5b3006a92e84148cd56fa439
ccarvajalhdez <ccarvajal@ic-itcr.ac.cr>
2016-06-06 19:17:52 -0600

    Main change.

--- src/main/java/com/itcr/demoscratos/Main.java
+++ src/main/java/com/itcr/demoscratos/Main.java
@@ -1,14 +1,9 @@
 package com.itcr.demoscratos;
 
-import com.itcr.demoscratos.mongodb.ConnectionMongo;
-
 public abstract class Main {
 
 	public Main() {	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
-
-	}
+		System.out.println("Hello World!");	}
 }
